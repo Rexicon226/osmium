d92366370bc36e63c4dac765c8ae58ba5868f317
David Rubin <daviru007@icloud.com>
2024-03-19 03:14:29 -0700

    remove test.zig

--- test.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    const V = @Vector(4, *const @Vector(4, u32));
-    const x: V = @splat(&@splat(10));
-    std.debug.print("x: {}\n", .{x});
-}
