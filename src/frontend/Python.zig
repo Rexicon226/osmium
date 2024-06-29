07fcc4c6671e8b037caff84315922d807beb5211
David Rubin <daviru007@icloud.com>
2024-06-29 08:11:02 -0700

    split off the cpython stuff into bindings

--- src/frontend/Python.zig
+++ src/frontend/Python.zig
@@ -5,7 +5,6 @@
 //! Inputs python source and outputs Bytecode
 
 pub const Error = error{
-    FailedToCompileString,
     FailedToWriteObjectToString,
     FailedToAsStringCode,
     BytesEmpty,
@@ -19,21 +18,21 @@ pub fn parse(
     const t = tracer.trace(@src(), "", .{});
     defer t.end();
 
-    try cpython.Initialize(allocator, build_options.lib_path);
+    try Initialize(allocator);
 
-    const compiled = cpython.CompileString(source, filename);
+    const compiled = helpers.CompileString(source, filename);
     if (null == compiled) {
-        cpython.PrintError();
+        helpers.PrintError();
         std.posix.exit(1);
     }
 
-    const bytecode = cpython.Marshal_WriteObjectToString(compiled);
+    const bytecode = helpers.Marshal_WriteObjectToString(compiled);
     if (null == bytecode) {
         return error.FailedToWriteObjectToString;
     }
 
-    const size = cpython.Bytes_Size(bytecode);
-    const ptr = cpython.Bytes_AsString(bytecode);
+    const size = helpers.Bytes_Size(bytecode);
+    const ptr = helpers.Bytes_AsString(bytecode);
     if (null == ptr) {
         return error.FailedToAsStringCode;
     }
@@ -53,10 +52,51 @@ pub fn parse(
     try writer.writeInt(u32, @intCast(source.len), .little);
     try writer.writeAll(pyc_bytes);
 
-    cpython.DecRef(bytecode);
-    cpython.Finalize();
+    helpers.DecRef(bytecode);
+    helpers.Finalize();
 
     return bytes;
+}
+
+pub fn utf8ToUtf32Z(
+    in: []const u8,
+    allocator: std.mem.Allocator,
+) ![:0]const u32 {
+    var buffer = std.ArrayList(u32).init(allocator);
+    for (in) |char| {
+        try buffer.append(char);
+    }
+    return buffer.toOwnedSliceSentinel(0);
+}
+
+pub fn Initialize(
+    allocator: std.mem.Allocator,
+) !void {
+    var config: types.PyConfig = undefined;
+    externs.PyConfig_InitPythonConfig(&config);
+    defer externs.PyConfig_Clear(&config);
+
+    // mute some silly errors that probably do infact matter
+    config.pathconfig_warnings = 0;
+
+    _ = externs.PyConfig_SetBytesString(
+        &config,
+        &config.program_name,
+        "osmium".ptr,
+    );
+    _ = externs.PyConfig_Read(&config);
+
+    const utf32_path = try utf8ToUtf32Z("../python/Lib", allocator);
+
+    config.module_search_paths_set = 1;
+    _ = externs.PyWideStringList_Append(
+        &config.module_search_paths,
+        utf32_path.ptr,
+    );
+
+    const status = externs.Py_InitializeFromConfig(&config);
+    // needs to be a pointer discard because the stack protector gets overrun?
+    _ = &status;
 }
 
 const MAGIC_NUMBER: u32 = 0xa0d0d6f;
@@ -66,6 +106,10 @@ const std = @import("std");
 
 const log = std.log.scoped(.python);
 
-const cpython = @import("cpython.zig");
+const cpython = @import("cpython");
+const types = cpython.types;
+const helpers = cpython.helpers;
+const externs = cpython.externs;
+
 const tracer = @import("tracer");
 const build_options = @import("options");
